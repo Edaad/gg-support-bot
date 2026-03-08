8773ac758ab7b1e034c590e34ce440960f4c6fe8
Edaad Azman <edaad@bu.edu>
2026-03-08 14:11:57 -0400

    gto support added to config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # config.py
 
 ADMIN_USER_IDS = [
-    493310710,   # edaad personal
+    493310710,  # edaad personal
     6713100304,  # rtsupport
     8318575265,  # ccsupport
+    7516419496,  # gtosupport
 ]
